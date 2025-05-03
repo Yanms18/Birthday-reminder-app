faf32e47db1c340634b3dfe792971a2fd6a07962
yanms18 <jejeoluwayanmife@gmail.com>
2025-05-03 12:03:50 +0100

    done

--- Birthdaytest.js
+++ Birthdaytest.js
@@ -8,11 +8,11 @@ const OAuth2 = google.auth.OAuth2;
 const oauth2Client = new OAuth2(
       process.env.GMAIL_CLIENT_ID, // ClientID
      process.env.GMAIL_CLIENT_SECRET, // Client Secret
-     "https://developers.google.com/oauthplayground" // Redirect URL
+     process.env.GMAIL_REFRESH_TOKEN// Redirect URL
 );
 
 oauth2Client.setCredentials({
-     refresh_token: "1//04TE2odWn2QwMCgYIARAAGAQSNwF-L9Ir1OZV0crBHSGTuI2_HypO3qUEiWv54jVBs76HcQkDao_h2TfokBp-cVuk1eMCgm-f_hY"
+     refresh_token: process.env.GMAIL_REFRESH_TOKEN
 });
 const accessToken = oauth2Client.getAccessToken();
 
@@ -52,7 +52,7 @@ router.get('/test-birthday-check', async (req, res) => {
 
       await Promise.all(birthdayUsers.map(user => {
         let mailOptions = {
-          from: 'jejeoluwayanmife@gmail.com',
+          from: process.env.GMAIL_USER,
           to: user.email,
           subject: 'Happy Birthday!',
           html: `<h1>Happy Birthday, ${user.username}!</h1>
